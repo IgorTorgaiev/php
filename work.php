a1893f20c0c97ef67c577ac8305e3de1c6121c97
IgorTorgaiev <51889185+IgorTorgaiev@users.noreply.github.com>
2020-08-18 16:26:03 +0200

    Add files via upload

---
+++ work.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Hello World!</h1>
+
+
+    <?php
+    echo "Hello World<br><br>"; // that is the row where the PHP code ought to be placed
+    echo "Nice to see you";
+    echo "<hr>";
+    ?>
+
+</body>
+</html>
